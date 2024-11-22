21b607d119e56bd6133338e5cd6cfd3053179e01
Carter Davis <cdavis4short@gmail.com>
2024-11-21 18:51:26 -0600

    linuxserver images

--- hosts/apoc/containers.nix
+++ hosts/apoc/containers.nix
@@ -1,25 +1,28 @@
 {
   virtualisation.oci-containers.containers = {
     media = {
-      image = "jellyfin/jellyfin:latest";
+      image = "linuxserver/jellyfin:latest";
       ports = [ "8000:8096" ];
+      extraOptions = [ "--device=/dev/dri:/dev/dri" ];
       volumes = [
         "/mnt/.containers/media:/config"
         "/mnt/media:/mnt/media"
       ];
+      environment = {
+        TZ = "America/Chicago";
+	JELLYFIN_PublishedServerUrl = "https://media.sonnygrace.net";
+      };
     };
     cloud = {
-      image = "nextcloud:stable";
+      image = "linuxserver/nextcloud:latest";
       ports = [ "8001:80" ];
       volumes = [
-        "/mnt/.containers/cloud/data:/var/www/html/data"
-        "/mnt/.containers/cloud/config:/var/www/html/config"
-        "/mnt/.containers/cloud/apps:/var/www/html/apps"
+        "/mnt/.containers/cloud/config:/config"
+        "/mnt/.containers/cloud/data:/data"
         "/mnt/media:/mnt/media"
       ];
       environment = {
-        OVERWRITEPROTOCOL = "https";
-        TRUSTED_PROXIES = "172.17.0.1";
+        TZ = "America/Chicago";
       };
     };
   };
